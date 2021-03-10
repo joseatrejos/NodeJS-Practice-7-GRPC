64553d3ab7f67adf4a3780686e6a0329944c7b1b
Jose Trejo <joseatrejos@gmail.com>
2021-03-09 22:15:21 -0700

    Done

--- producer-grpc.js
+++ producer-grpc.js
@@ -1,7 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const loader = require('@grpc/proto-loader');
 
-const pkg_definition = loader.loadSync(_dirname + "/receta.proto");
+const pkg_definition = loader.loadSync(__dirname + "/receta.proto");
 const receta = grpc.loadPackageDefinition(pkg_definition).receta;
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
@@ -14,8 +14,32 @@ server.addService(receta.ServicioReceta.service, {
         });
     },
     obtenerReceta: (call, cb) => {
+        if(call.request.id !== 42) {
+            return cb(new Error(`No se encontró la receta: ${call.request.id }`));
+        }
         cb(null, {
-            pid: process.pid
+            id: 42,
+            nombre: "Tacos de pollo",
+            pasos: "Toma una tortilla y échale pollo",
+            ingredientes: [
+                {
+                    id: 1,
+                    nombre: "Pollo",
+                    cantidad: "100grs"
+                },
+                {
+                    id: 2,
+                    nombre: "Tortillas",
+                    cantidad: "2 piezas"
+                },
+
+            ]
         });
     }
+});
+
+server.bindAsync(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if(err) throw err;
+    server.start();
+    console.log(`Server iniciado en: http://${HOST}:${PORT}/`);
 });
